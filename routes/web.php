2a2766f1cb6540cb0cf9a440fd2c4f659f35cfa4
Fabiano <radbios@gmail.com>
2025-03-15 20:54:28 -0300

    refactor: organize views into guest and auth sections for better structure

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware("guest")->group(function(){
-    Route::view("login", "login")->name("login");
-    Route::view("register", "register")->name("register");
+    Route::view("guest.login", "login")->name("login");
+    Route::view("guest.register", "register")->name("register");
 
     Route::post("register", [UserController::class, "register"])->name("user.register");
     Route::post("authenticate", [AuthController::class, "authenticate"])->name("authenticate");
@@ -17,6 +17,6 @@ Route::middleware("auth")->group(function(){
     Route::post("logout", [AuthController::class, "logout"])->name("logout");
 
     Route::get("/", function(){
-        return view("home");
+        return view("auth.home");
     })->name("home");
 });
