80fd2444fd4d827511f1b9a507950af71ab5f158
Fabiano <radbios@gmail.com>
2025-03-15 19:16:51 -0300

    feat: creating main layout

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="{{asset("assets/css/main.css")}}">
+    <link rel="stylesheet" href="{{asset("mdb/css/mdb.min.css")}}">
+    
+    @yield("css")
+
+    <title>@yield("title") | {{config("app.name")}}</title>
+</head>
+
+<body>
+    @yield("container")
+</body>
+
+<script src="{{asset("mdb/js/mdb.es.min.js")}}"></script>
+<script src="{{asset("mdb/js/mdb.umd.min.js")}}"></script>
+
+@yield("js")
+</html>
