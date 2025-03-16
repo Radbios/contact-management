1c477a744808486f4a07c95657b06c219dd2261b
Fabiano <radbios@gmail.com>
2025-03-15 22:34:38 -0300

    fix: adjustment route view

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware("guest")->group(function(){
-    Route::view("guest.login", "login")->name("login");
-    Route::view("guest.register", "register")->name("register");
+    Route::view("login", "guest.login")->name("login");
+    Route::view("register", "guest.register")->name("register");
 
     Route::post("register", [UserController::class, "register"])->name("user.register");
     Route::post("authenticate", [AuthController::class, "authenticate"])->name("authenticate");
